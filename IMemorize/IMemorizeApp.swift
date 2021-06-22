38c9079a2cf2d7d2248006d734b1515ab974c2be
iranildo <iranildocarlos@gmail.com>
2021-06-21 23:42:02 -0300

    Initial Commit

---
+++ IMemorize/IMemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMemorizeApp.swift
+//  IMemorize
+//
+//  Created by Iranildo Carlos da Silva on 21/06/21.
+//
+
+import SwiftUI
+
+@main
+struct IMemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
